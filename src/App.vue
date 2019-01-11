1a07536c4ed2246b473df2b4e9acdde1bd72cd3b
sourya <soury.bunny@gmail.com>
2019-01-11 13:54:14 +0530

    add and delete quotes functionality

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container">
+        <app-new-quote @quoteAdded = "createQuote"></app-new-quote>
+        <app-quote-grid :quotes = "quotes" @quoteDeleted = "deleteQuote"></app-quote-grid>
+        <div class="row">
+            <div class= "col-sm-12 text-center">
+                <div class="alert alert-info">Info: Click on a quote to delete it.</div>
+            </div>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+import QuoteGrid from './components/QuoteGrid.vue';
+import NewQuote from './components/NewQuote.vue';
+    export default {
+        data(){
+            return {
+                quotes: ['somethign'],
+                maxQuotes: 10
+            }
+        },
+        components: {
+            'appQuoteGrid': QuoteGrid,
+            'appNewQuote': NewQuote
+        },
+        methods: {
+            createQuote(quote){
+                if(this.quotes.length < this.maxQuotes){
+                if(quote){
+                    this.quotes.push(quote);
+                }else {
+                    alert("please enter some text")
+                }
+                }else {
+                    alert("exceeded max quotes, delete a few.")
+                }
+            },
+            deleteQuote(index){
+                this.quotes.splice(index,1);
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
